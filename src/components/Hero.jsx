d2356eda5e323487a7d6d7a62e7900a921db1ff7
Arnold Waigwa <waigwaarnold@gmail.com>
2024-10-04 23:01:33 -0400

    Added more functionalites: Added ability to input guess through a form, enabled to shuffle cards. Changed style to confirm if right or wrong. Current streak and longest streak being tracked

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -61,15 +61,27 @@ const Hero = () => {
     },
   ];
 
+  const [userAnswer, setUserAnswer] = useState("");
+
   const [currentCardIndex, setCurrentCardIndex] = useState(0);
 
   const [isFlipped, setIsFlipped] = useState(false);
+
+  const [answers, setAnswers] = useState([]);
+
+  const [currentStreak, setCurrentStreak] = useState(0);
+
+  const [longestStreak, setLongestStreak] = useState(0);
+
+  const [isCorrect, setIsCorrect] = useState(null);
 
   const handleNextCard = () => {
     setIsFlipped(false); // Reset card flip state to show front
     setCurrentCardIndex(
       (prevIndex) => (prevIndex + 1) % triviaQuestions.length
     ); // Increment index
+    setUserAnswer("");
+    setIsCorrect(null);
   };
 
   const handlePreviousCard = () => {
@@ -77,16 +89,60 @@ const Hero = () => {
     setCurrentCardIndex(
       (prevIndex) => (prevIndex - 1) % triviaQuestions.length
     );
+    setUserAnswer("");
+    setIsCorrect(null);
   };
 
   const handleFlip = () => {
     setIsFlipped(!isFlipped);
   };
 
+  const handleChange = (e) => {
+    setUserAnswer(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const currentQuestion = triviaQuestions[currentCardIndex];
+
+    if (
+      userAnswer.trim().toLowerCase() ===
+      currentQuestion.answer.trim().toLowerCase()
+    ) {
+      //answer is correct
+      setAnswers([...answers, userAnswer]);
+      setCurrentStreak((prevStreak) => prevStreak + 1);
+      setLongestStreak((prevLongest) =>
+        Math.max(prevLongest, currentStreak + 1)
+      );
+      setIsCorrect(true);
+    } else {
+      //answer is wrong
+      setCurrentStreak(0);
+      setIsCorrect(false);
+    }
+  };
+
+  const randomQuestion = () => {
+    const randomIndex = Math.floor(Math.random() * triviaQuestions.length);
+    setCurrentCardIndex(randomIndex);
+    setIsFlipped(false);
+    setUserAnswer("");
+    setIsCorrect(null);
+  };
+
+  const inputStyle = {
+    borderColor: isCorrect === null ? "#ccc" : isCorrect ? "green" : "red",
+    borderWidth: "2px",
+  };
+
   return (
     <div>
       <p>
         <strong>Number of cards: 10</strong>
+        <p>
+          Current Streak: {currentStreak} , Longest Streak: {longestStreak}
+        </p>
       </p>
       <div
         onClick={handleFlip}
@@ -100,11 +156,34 @@ const Hero = () => {
         <div className="back">{triviaQuestions[currentCardIndex].answer}</div>
       </div>
       <div>
-        <button onClick={handleNextCard} className="nextCard">
+        <br />
+        <div>
+          <form onSubmit={handleSubmit}>
+            <label>Guess the answer here:</label>
+            <input
+              className="correct"
+              type="text"
+              placeholder="Place your answer here"
+              value={userAnswer}
+              onChange={handleChange}
+              style={inputStyle}
+            />
+            <button type="submit">Submit Guess</button>
+          </form>
+        </div>
+        <button onClick={handlePreviousCard} className="nextCard">
           Previous
         </button>
-        <button onClick={handlePreviousCard} className="nextCard">
+        <button onClick={handleNextCard} className="nextCard">
           Next
+        </button>
+        <button
+          onClick={() => {
+            randomQuestion;
+          }}
+          className="nextCard"
+        >
+          Shuffle
         </button>
       </div>
     </div>
